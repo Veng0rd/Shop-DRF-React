5f7a3bfef221ed5dae8dcce60c1f8a07591c22ff
Vengord <artomkolomeec@gmail.com>
2023-11-15 04:07:56 +0600

    Add docker with postgresql

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l@ug8lhl(l9!qn#j_md%$l2^*mm7#nh^08r@swo1uicgnzne9z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,9 +75,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "USER": "shop_user",
+    #     "NAME": "shop_db",
+    #     "PASSWORD": "root",
+    #     "HOST": "localhost",
+    # },
 }
 
 
