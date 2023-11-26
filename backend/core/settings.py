7817955aa0c4199cd4f6a30f6d05556b1381ca31
Vengord <artomkolomeec@gmail.com>
2023-11-26 23:03:06 +0600

    Edit corsheaders

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,19 +45,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
-
-CORS_ORIGIN_ALLOW_ALL = False
-
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:5173',
-)
 
 ROOT_URLCONF = 'core.urls'
 
@@ -151,3 +145,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = (
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+)
+
+CORS_ALLOW_CREDENTIALS = True
