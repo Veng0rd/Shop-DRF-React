bb6443dfb47f8684151da8b69842dae9320021a4
Farkk <blazem.show@mail.ru>
2023-12-07 00:37:08 +0600

    fix-search

--- frontend/src/modules/product/api/productApi.ts
+++ frontend/src/modules/product/api/productApi.ts
@@ -2,5 +2,6 @@ export const requestProduct = async (id: string) => {
   const response = await fetch(`http://localhost:8000/api/v1${id}`)
 
   const data = await response.json()
+  console.log(data)
   return data
 }
--- frontend/src/pages/productPage/ProductPage.tsx
+++ frontend/src/pages/productPage/ProductPage.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 import { Product } from '../../modules/product/index'
 
+import styles from './productPage.module.css'
+
 const ProductPage = () => {
+  useEffect(() => {
+    // Добавление класса к body при монтировании компонента
+    document.body.classList.add(`${styles.hidden}`)
+
+    // Очистка при размонтировании (если нужно)
+    return () => {
+      document.body.classList.remove(`${styles.hidden}`)
+    }
+  }, [])
   return (
     <div className="modalDiv">
       <div className="modal">
